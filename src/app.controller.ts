8619d19685de73ebbeb3c6a9522ee19aca72b872
lucasjstn <lucas7justino@gmail.com>
2023-01-31 20:10:26 -0300

    setting the views/layouts as handlebars partials

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
-    return '<b>Hello Worasdsdsdfld!</b>';
-  }
+  @Get('/') // you can pass routes to this method decorator
+  @Render('index')
+  index() {}
 }
+
+// In summary, once a user visits the “/” route, Nest will execute the AppController index method, which will render
+// the index view
