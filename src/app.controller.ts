818b1577d4ebdab302cc195607962312e74c52b8
lucasjstn <lucas7justino@gmail.com>
2023-02-01 07:26:29 -0300

    creating the about and index views

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,26 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/') // you can pass routes to this method decorator
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Store',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['description'] = 'This is an about page...';
+    viewData['author'] = 'Developed by: Lucas jstn';
+    const data1 = 'About us - Online Store';
+
+    return {
+      title: data1,
+      subtitle: 'About us',
+      viewData: viewData,
+    };
+  }
 }
 
 // In summary, once a user visits the “/” route, Nest will execute the AppController index method, which will render
