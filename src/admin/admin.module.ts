17aaaa67a7984974edaec04bfa9efe293319134a
lucasjstn <lucas7justino@gmail.com>
2023-02-02 18:52:02 -0300

    admin panel showing the current products

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
-
+import { AdminProductsController } from './admin.products.controller';
+import { ProductsService } from 'src/models/products.service';
 @Module({
-  controllers: [AdminController],
+  controllers: [AdminController, AdminProductsController],
 })
 export class AdminModule {}
