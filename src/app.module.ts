37414b1f1036a8203d295d531d2f5d3e7ef47184
lucasjstn <lucas7justino@gmail.com>
2023-02-04 22:49:28 -0300

    login logout system

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './models/products.service';
 import { Product } from './models/product.entity';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { User } from './models/user.entity';
+import { UsersService } from './models/user.service';
 
 @Global()
 @Module({
@@ -19,11 +22,12 @@ import { AdminModule } from './admin/admin.module';
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([Product, User]),
     AdminModule,
+    AuthModule,
   ],
   controllers: [AppController, ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  providers: [ProductsService, UsersService],
+  exports: [ProductsService, UsersService],
 })
 export class AppModule {}
